13a538ecc952cd85716cae62bd12cbbf77b982b7
Lia <liazerqueraf@gmail.com>
2023-11-14 18:05:30 -0500

    create emojie cards view and buttons

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,15 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
-var emojies: [String] = ["😆","😍","🌊", "🤩","😆","😍","🌊", "🤩"]
+    var emojies: [String] = ["😆","😍","🌊", "🤩","😆","😍","🌊", "🤩"]
+    @State var countCard: Int = 4
     var body: some View {
+        
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            HStack{
+                addMinusButton
+                Spacer()
+                addPlusButton
+            }
+        }
+        .font(.largeTitle)
+        .padding()
+        
+        
+    }
+    var cards: some View{
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))], content: {
-            ForEach(0..<emojies.count){
+            ForEach(0..<countCard, id: \.self){
                 index in CardView(emojie: emojies[index])
             }
             
         })
+    }
+    func createButton(count: Int, action_name: String) -> some View{
+        Button(action:{
+            countCard += count
+        }, label: {
+            Image(systemName: action_name)
+            
+        }).imageScale(.large)
         
+    }
+    var addPlusButton: some View{
+        return createButton(count: +1, action_name:"rectangle.stack.fill.badge.plus" ).disabled(countCard >= emojies.count)
+    }
+    var addMinusButton: some View{
+    
+        return createButton(count: -1, action_name: "rectangle.stack.fill.badge.minus").disabled(countCard == 1)
     }
 }
 
@@ -26,20 +60,24 @@ struct CardView: View {
     var body: some View {
         ZStack{
             if faceUp{
-                RoundedRectangle(cornerRadius: 12).foregroundColor(.white)
-                    .border(Color.orange)
+                RoundedRectangle(cornerRadius: 25).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 25).strokeBorder(lineWidth: /*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
             }
             else{
-                RoundedRectangle(cornerRadius: 12.0).foregroundColor(.orange)
+                RoundedRectangle(cornerRadius: 25.0)
             }
            
             Text(emojie).font(.largeTitle).opacity(faceUp ? 1: 0 )
         }
+        .padding(0.0)
+        .frame(width: 150.0, height: 200.0)
+        .foregroundColor(.orange)
         .onTapGesture {
             faceUp.toggle()
         }
     }
 }
+    
 #Preview {
     ContentView()
 }
