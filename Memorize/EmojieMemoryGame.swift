7b231ad1bf0f625b09a25fdda480453b08f1c9ca
Lia <liazerqueraf@gmail.com>
2024-02-07 01:00:16 -0500

    Creations of View Model

--- Memorize/EmojieMemoryGame.swift
+++ Memorize/EmojieMemoryGame.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+
+class EmojieMemoryGame : ObservableObject{
+    private static let emojie: [String] =  ["😆", "😍", "🤩", "😆", "😍", "🤩", "😃", "😅", "🥲", "🙃", "🙂", "😉", "😌"]
+    private static let cars : [String] = ["🚗", "🏎️", "🛻", "🚕", "🚓", "🚚", "🚙", "🚑", "🚛", "🚌", "🚒", "🚎", "🚐", "🚔", "🚍", "🚘", "🚖", "🛵","🚗", "🏎️", "🛻", "🚕", "🚓", "🚚", "🚙", "🚑", "🚛", "🚌", "🚒", "🚎", "🚐", "🚔", "🚍", "🚘", "🚖", "🛵","🚗", "🏎️", "🛻", "🚕", "🚓", "🚚", "🚙", "🚑", "🚛", "🚌", "🚒", "🚎", "🚐", "🚔", "🚍", "🚘", "🚖", "🛵"]
+    private static let sport : [String] = ["⚽️", "🏀", "🎾", "🏐", "🏈", "🥏", "⚾️", "🏓", "🏸", "🥅", "🥋", "🥊", "🥍", "⛳️"]
+    
+    private static func createMemoryGame() -> MemorizeGame<String> {
+        return   MemorizeGame<String>(numberOfPair : 3){
+            index in
+            if emojie.indices.contains(index){
+                return emojie[index]
+            }
+            else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+
+    
+    var cards: Array<MemorizeGame<String>.Card> {
+        return model.cards
+    }
+    
+    func choose(card :MemorizeGame<String>.Card){
+        model.choose(card)
+        
+    }
+    func shuffle(){
+        model.shuffle()
+
+    }
+}
